e2de2b808ac46a3c23f039fe31d064de5dc7a2df
Jeff Lane <jeff@jeffreyrobertlane.com>
2025-05-01 17:34:20 -0700

    adding dark mode for cart slider

--- src/app/_components/shopping-cart/AddToCartSlider.js
+++ src/app/_components/shopping-cart/AddToCartSlider.js
@@ -45,7 +45,7 @@ function AddToCartSlider() {
     <Dialog open={openCart} onClose={handleCloseCart} className="relative z-10">
       <DialogBackdrop
         transition
-        className="fixed inset-0 bg-gray-500/75 transition-opacity duration-500 ease-in-out data-closed:opacity-0"
+        className="fixed inset-0 bg-gray-500/75 dark:bg-primary-900/75 transition-opacity duration-500 ease-in-out data-closed:opacity-0"
       />
 
       <div className="fixed inset-0 overflow-hidden">
@@ -55,17 +55,17 @@ function AddToCartSlider() {
               transition
               className="pointer-events-auto w-screen max-w-md transform transition duration-500 ease-in-out data-closed:translate-x-full sm:duration-700"
             >
-              <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+              <div className="flex h-full flex-col overflow-y-scroll bg-white dark:bg-primary-950 shadow-xl">
                 <div className="flex-.75 overflow-y-auto px-4 py-6 sm:px-6">
                   <div className="flex items-start justify-between">
-                    <DialogTitle className="text-lg font-medium text-gray-900">
+                    <DialogTitle className="text-lg font-medium text-gray-900 dark:text-primary-100">
                       Added to your cart
                     </DialogTitle>
                     <div className="ml-3 flex h-7 items-center">
                       <button
                         type="button"
                         onClick={handleCloseCart}
-                        className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
+                        className="relative -m-2 p-2 text-gray-400 dark:text-primary-200 hover:text-gray-500"
                       >
                         <span className="absolute -inset-0.5" />
                         <span className="sr-only">Close panel</span>
@@ -76,9 +76,9 @@ function AddToCartSlider() {
 
                   <div className="mt-8">
                     <div className="flow-root">
-                      <div className="-my-6 divide-y divide-gray-200">
+                      <div className="-my-6 divide-y divide-gray-200 dark:divide-primary-700">
                         <div className="flex py-6">
-                          <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
+                          <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200 dark:border-primary-900">
                             {cartItem && cartItem?.image && (
                               <Image
                                 alt={cartItem.title}
@@ -92,7 +92,7 @@ function AddToCartSlider() {
 
                           <div className="ml-4 flex flex-1 flex-col">
                             <div>
-                              <div className="flex justify-between text-base font-medium text-gray-900">
+                              <div className="flex justify-between text-base font-medium text-gray-900 dark:text-primary-200">
                                 <h3>
                                   {cartItem &&
                                     cartItem?.title &&
@@ -105,7 +105,7 @@ function AddToCartSlider() {
                                     formatDollars(cartItem.price)}
                                 </p>
                               </div>
-                              <p className="mt-1 text-sm text-gray-500">
+                              <p className="mt-1 text-sm text-gray-500 dark:text-primary-300">
                                 {cartItem &&
                                   cartItem?.recordFormat &&
                                   printRecordFormat(cartItem.recordFormat)}{" "}
@@ -115,7 +115,7 @@ function AddToCartSlider() {
                               </p>
                             </div>
                             <div className="flex flex-1 items-end justify-between text-sm">
-                              <p className="text-gray-500">
+                              <p className="text-gray-500 dark:text-primary-300">
                                 Qty{" "}
                                 {cartItem && cartItem?.count && cartItem.count}
                               </p>
@@ -129,8 +129,8 @@ function AddToCartSlider() {
                   </div>
                 </div>
 
-                <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-                  <div className="flex justify-between text-base font-medium text-gray-900">
+                <div className="border-t border-gray-200 dark:border-primary-700 px-4 py-6 sm:px-6">
+                  <div className="flex justify-between text-base font-medium text-gray-900 dark:text-primary-100">
                     <p>Subtotal</p>
                     <p>
                       $
@@ -140,7 +140,7 @@ function AddToCartSlider() {
                         formatDollars(cartItem.price * cartItem.count)}
                     </p>
                   </div>
-                  <p className="mt-0.5 text-sm text-gray-500">
+                  <p className="mt-0.5 text-sm text-gray-500 dark:text-primary-300">
                     Shipping and taxes calculated at checkout.
                   </p>
                   <div className="mt-6">
@@ -151,13 +151,13 @@ function AddToCartSlider() {
                       View Cart
                     </button>
                   </div>
-                  <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                  <div className="mt-6 flex justify-center text-center text-sm text-gray-500 dark:text-primary-300">
                     <p>
                       or{" "}
                       <button
                         type="button"
                         onClick={handleContinueShopping}
-                        className="font-medium text-accent-600 hover:text-accent-500 hover:cursor-pointer"
+                        className="font-medium text-accent-600 hover:text-accent-500 hover:cursor-pointer dark:text-accent-500 dark:hover:text-accent-400"
                       >
                         Continue Shopping
                         <span aria-hidden="true"> &rarr;</span>
