9c088fbdc80624a34440ee69c2dd316798791756
Jeff Lane <jlaneconsulting@gmail.com>
2025-07-03 12:08:15 -0700

    removing email field.  Should only be changed under account/profile

--- src/app/_components/checkout/Checkout.js
+++ src/app/_components/checkout/Checkout.js
@@ -311,26 +311,6 @@ function Checkout({ cart, countries }) {
               />
             )}
 
-            <div className="mt-6">
-              <label
-                htmlFor="email-address"
-                className="block text-sm/6 font-medium text-gray-700"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email-address"
-                  name="email-address"
-                  type="email"
-                  autoComplete="email"
-                  value={email}
-                  ref={emailRef}
-                  onChange={handleEmail}
-                  className="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-accent-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
             {!showPayPalButtons && (
               <div className="mt-6 justify-center w-full flex">
                 <button
