6b0361f7d6ae8478b6e7fbfd46d52bc96e33d19a
Jeff Lane <jlaneconsulting@gmail.com>
2025-07-22 12:38:43 -0700

    adding feature flag for checkout to hide it in prod

--- src/app/checkout/payment/page.js
+++ src/app/checkout/payment/page.js
@@ -4,12 +4,25 @@ import {
   serverGetCountries,
   serverGetShoppingCart,
 } from "@/src/app/_library/serverActions";
+import ComingSoonSmall from "../../_components/utilities/ComingSoonSmall";
 const revalidate = 0;
 async function Page() {
   const { data: cart } = await serverGetShoppingCart();
   const { data: countries } = await serverGetCountries();
   // console.log(cart);
   if (!cart) return <CartIsEmpty />;
+  if (process.env.HIDE_CHECKOUT === "true")
+    return (
+      <>
+        <ComingSoonSmall />
+        <div className="flex grid-cols-3">
+          <div className="w-1/3"></div>
+          <div className="w-1/3 text-center text-5xl mt-10">Checkout</div>
+          <div className="w-1/3"></div>
+        </div>
+      </>
+    );
+
   return (
     <div>
       <Checkout cart={cart} countries={countries} />
